96145b05c9c2c55c0bcefb032638fb9a355d8a9d
sriram806 <boddusriram1234@gmail.com>
2026-02-22 18:56:06 +0530

    client: Add Profile UI

--- client/next.config.ts
+++ client/next.config.ts
@@ -22,6 +22,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
       }
     ],
   },
